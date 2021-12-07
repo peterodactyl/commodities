29b093e9d03019d06b5bde91c3960772b3240ef6
Pedro Caiafa <caiafapedro@gmail.com>
2021-12-06 22:24:13 -0300

    first commit

---
+++ paths.py
@@ -0,0 +1,4 @@
+from os import path
+
+path_com = r'C:\Users\PerdoCaiafa\Documents\MAUA\MINOR\PROJETOS\historical_data_nasdaq'
+path_debt = r'C:\Users\PerdoCaiafa\Documents\MAUA\MINOR\PROJETOS\DebtPenny_19930401_20210927'
